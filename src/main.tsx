077409d27eb45a64cef50da34fd08e691924fe82
Houssein Msallem <houssein.msallem@gmail.com>
2024-10-25 16:53:25 +0100

    nonn

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 import Router from './Router';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <Router />
+    <Router />
   </StrictMode>
 );
